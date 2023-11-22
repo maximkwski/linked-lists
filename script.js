429d5e6a702504d7785bf719a403d460ac346467
Maksym <makkie.makowski@gmail.com>
2023-11-22 15:31:08 -0500

    add pop removeAt insertAt methods

--- script.js
+++ script.js
@@ -19,6 +19,26 @@ class LinkedList {
         return this.size
     }
 
+    getHead() {
+        if(this.head) {
+            return this.head
+        } else {
+            console.log('the list is empty')
+        }
+    }
+
+    getTail() {
+        if (this.isEmpty()) {
+            console.log('the list is empty')
+        } else {
+            let prev = this.head
+            while (prev.next) {
+                prev = prev.next
+            }
+            return prev
+        }
+    }
+
     prepend(value) {
         const node = new Node(value)
         if (!this.isEmpty()){
@@ -57,6 +77,77 @@ class LinkedList {
             console.log(listValues)
         }
     }
+
+    at(index) {
+        let i = 0
+        if (this.isEmpty()) {
+            console.log('list is empty')
+        } else {
+            let curr = this.head
+            while (i < index) {
+                curr = curr.next
+                i++
+                console.log(curr)
+            }
+            return curr
+        }
+        
+    }
+
+    insertAt(value, index) {
+        if (index < 0 || index > this.size) {
+            return
+        } 
+        if (index === 0) {
+            list.prepend(value)
+        } else {
+            let prev  = this.head
+            const node = new Node(value)
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            node.next = prev.next
+            prev.next = node
+        }
+      
+    }
+
+    removeAt(index) {
+        if (index < 0 || index > this.size) {
+            return null
+        } 
+        let removedNode
+        if (index === 0) {
+            removedNode = this.head
+            this.head = this.head.next
+        } else {
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            removedNode = prev.next
+            prev.next = removedNode.next
+        }
+        this.size++
+        return removedNode.value
+    }
+
+    pop() {
+        if (this.isEmpty()){
+            console.log('the list is empty')
+        } else {
+            let prev = this.head
+            let removedNode
+            for (let i = 0; i < this.size - 1; i++) {
+                prev = prev.next
+            }
+            removedNode = prev.next
+            prev.next = null
+            return removedNode.value
+            
+        
+        }
+    }
 }
 
 const list = new LinkedList();
@@ -68,3 +159,7 @@ list.append('tam');
 
 list.print();
 console.log(list);
+list.insertAt('kaban', 1);
+list.print();
+console.log('prev is ', list.pop());
+list.print();
